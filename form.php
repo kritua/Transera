bf27d3e0be1b454340a081803d7f52cdd8d84748
Kri Tua <kritua2@gmail.com>
2017-01-18 08:43:47 +0300

    Update

--- form.php
+++ form.php
@@ -2,9 +2,9 @@
 
 session_start();
 
-if((time() - $_SESSION['send']) < 1) exit(json_encode(array('res' => 'info', 'msg' => 'Вы уже отправили заявку. Наши менеджеры свяжутся с вами')));
+if ((time() - $_SESSION['send']) < 600) exit(json_encode(array('res' => 'info', 'msg' => 'Вы уже отправили заявку. Наши менеджеры свяжутся с вами')));
 /*if(empty($_POST['address'])) exit(json_encode(array('res' => 'danger', 'msg' => 'Укажите тип груза')));*/
-if(empty($_POST['phone'])) exit(json_encode(array('res' => 'danger', 'msg' => 'Укажите номер телефона')));
+if (empty($_POST['phone'])) exit(json_encode(array('res' => 'danger', 'msg' => 'Укажите номер телефона')));
 
 
 $ip = $_SERVER['REMOTE_ADDR'];
@@ -15,30 +15,33 @@ $city = $info['city']['name_ru'];
 $country = $info['country']['name_ru'];
 
 $body =
-		($_POST['source'] ? "\nИсточник: {$_POST['source']}" : null).
-		($_POST['name'] ? "\nИмя: {$_POST['name']}" : null).
-		($_POST['phone'] ? "\nТелефон: {$_POST['phone']}" : null).
-		($_POST['mail'] ? "\nПочта: {$_POST['mail']}" : null).
-		($_POST['from'] ? "\nОткуда: {$_POST['from']}" : null).
-    ($_POST['to'] ? "\nКуда: {$_POST['to']}" : null).
-		($_POST['message'] ? "\nВопрос: {$_POST['message']}" : null).
-		($_POST['utm_source'] ? "\nutm_source: {$_POST['utm_source']}" : null).
-		($_POST['utm_medium'] ? "\nutm_medium: {$_POST['utm_medium']}" : null).
-		($_POST['utm_term'] ? "\nutm_term: {$_POST['utm_term']}" : null).
-		($_POST['utm_campaign'] ? "\nutm_campaign: {$_POST['utm_campaign']}" : null).
-        "\nМестонахождение: {$city} ({$ip})";
+    ($_POST['source'] ? "\nИсточник: {$_POST['source']}" : null) .
+    ($_POST['name'] ? "\nИмя: {$_POST['name']}" : null) .
+    ($_POST['phone'] ? "\nТелефон: {$_POST['phone']}" : null) .
+    ($_POST['mail'] ? "\nПочта: {$_POST['mail']}" : null) .
+    ($_POST['from'] ? "\nОткуда: {$_POST['from']}" : null) .
+    ($_POST['to'] ? "\nКуда: {$_POST['to']}" : null) .
+    ($_POST['message'] ? "\nВопрос: {$_POST['message']}" : null) .
+    ($_POST['utm_source'] ? "\nutm_source: {$_POST['utm_source']}" : null) .
+    ($_POST['utm_medium'] ? "\nutm_medium: {$_POST['utm_medium']}" : null) .
+    ($_POST['utm_term'] ? "\nutm_term: {$_POST['utm_term']}" : null) .
+    ($_POST['utm_campaign'] ? "\nutm_campaign: {$_POST['utm_campaign']}" : null) .
+    "\nМестонахождение: {$city} ({$ip})";
 
 include 'classes/mailer/PHPMailerAutoload.php';
 $mail = new PHPMailer;
 $mail->CharSet = 'UTF-8';
 $mail->setFrom('info@sts.ru', 'SpecTransService', $_POST['source']);
 $mail->addAddress('kritua2@gmail.com');
-$mail->Subject = $_POST['type'] ? $_POST['type'] : 'Заявка с сайта '.$_POST['source'];
-$mail->Body    = $body;
+$mail->addAddress('pro-sts.ru@yandex.ru');
+$mail->Subject = $_POST['type'] ? $_POST['type'] : 'Заявка с сайта ' . $_POST['source'];
+$mail->Body = $body;
 
-if($mail->send()) {
-	$_SESSION['send'] = time();
-	exit(json_encode(array('res' => 'success', 'msg' => 'Запрос успешно отправлен')));
+if ($mail->send()) {
+    $_SESSION['send'] = time();
+    exit(json_encode(array('res' => 'success', 'msg' => 'Запрос успешно отправлен')));
 }
 
 ?>
+
+
